fd0c66be295b9445d2b1bde53e7a0f5e9bcdb6fa
Brandon Olivier <Brandon.Olivier42@gmail.com>
2014-09-16 17:33:07 -0500

    Presentation 1.0 version

--- RProject/03Analysis/AccidentAnalysis.R
+++ RProject/03Analysis/AccidentAnalysis.R
@@ -19,7 +19,7 @@ head(accidents)
 
 # It's not the weather that's doing this
 # 1 is clear, 2 is rainy
-ggplot(data = (accidents)) + geom_histogram(aes_string(x = 'WEATHER')) + xlim(0, 8)
+ggplot(data = subset(accidents, WEATHER1 <= 8)) + geom_histogram(aes_string(x = 'WEATHER1')) + scale_x_discrete(limits = c("Clear", "Rain", "Hail", "Snow", "Fog", "Crosswind", "Sand Storm", "other"))
 
 # Most accidents happen on weekend, but not as many as we expected.
 ggplot(data = (accidents)) + geom_histogram(aes_string(x = 'DAY_WEEK')) 
@@ -34,15 +34,17 @@ ggplot(data = (accidents)) + geom_histogram(aes_string(x = 'MINUTE')) + xlim(0, 
 # People crash waaaaay more on the east coast...there are no people in the midwest
 ggplot(accidents) + geom_point(aes(x=LONGITUD,y=LATITUDE)) + coord_map(project="mercator") + xlim(-130, -60) + ylim(20, 55)
 
+b = map_data("state")
+
 png("occupants_map.png", width=2000, height=2000, res=120)
 map <- ggplot(accidents)
-map = map + geom_point(aes(x=LONGITUD,y=LATITUDE,color=PERSONS), size=0.4)
-map = map + coord_map(project="mercator") + xlim(-123, -68) + ylim(24, 50)
+map = map + geom_point(aes(x=LONGITUD,y=LATITUDE,color=PERSONS), size=2.0)
+map = map + coord_map(project="mercator") + xlim(-125, -115) + ylim(32, 45)
 map = map + scale_color_gradient(limits=c(0, 4), low = "yellow", high = "blue")
+map = map + geom_path(data=b, aes(x=long,y=lat,group=group), colour="black", alpha=0.5)
 map
 dev.off()
 
-b = map_data("state")
 png("occupants_fatals_map_texas.png", width=2000, height=2000, res=120)
 map <- ggplot(accidents)
 map = map + geom_point(aes(x=LONGITUD,y=LATITUDE,size=FATALS,color=FATALS))
@@ -59,7 +61,7 @@ hosp_arrival
 png("hospital_travel_time.png", width=2000, height=2000, res=120)
 map <- ggplot(accidents)
 map = map + geom_point(aes(x=LONGITUD,y=LATITUDE,color=HOSP_TRAVEL_TIME), size=0.9)
-map = map + coord_map(project="mercator") + xlim(-123, -68) + ylim(24, 50)
+map = map + coord_map(project="mercator") + xlim(-107, -93) + ylim(26, 37)
 map = map + scale_color_gradient(limits=c(1, 120), low = "blue", high = "red")
 map = map + geom_path(data=b, aes(x=long,y=lat,group=group), colour="black", alpha=0.5)
 map
@@ -69,5 +71,5 @@ dev.off()
 ggplot(accidents, aes(x = MONTH)) + geom_histogram(binwidth=0.5)
 
 # Time of day -- Need more lights
-qplot(LGT_COND, data=accidents, geom="bar") + xlim("Daylight", "Dark-Not Lighted", "Dark-Lighted", "Dawn", "Dusk", "Dark-Unkn. Lighting", "Other", "Not Reported", "Unknown")
+qplot(LGT_COND, data=accidents, geom="bar") + xlim("Day", "Dark-NL", "Dark-L", "Dawn", "Dusk", "Dark-?L", "Other", "?", "Unknown")
 
